a21bee72034d3e5071ec3ff780d43871263b2331
Nitin308 <nitin3081998@gmail.com>
2022-12-01 17:24:54 +0530

    StockAccountManagement

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ObjectOrientedPrograms.Repository;
+
+namespace ObjectOrientedPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Object Oriented Program\n");
+
+            StockManager stockManager = new StockManager();
+            stockManager.CalcStockValue();
+        }
+    }
+}
